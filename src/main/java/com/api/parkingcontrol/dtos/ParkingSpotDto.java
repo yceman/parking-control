ad8b755cd8202e1ac9e50161054de352f27d2666
Marcos Morais <marcosmorais.v10@gmail.com>
2022-11-10 11:40:13 -0300

    controllers

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,25 @@
+package com.api.parkingcontrol.dtos;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+public @Data class ParkingSpotDto {
+	@NotBlank
+	private String parkingSpotNumber;
+	@NotBlank
+	@Size(max = 7)
+	private String licensePlateCar;
+	@NotBlank
+	private String brandCar;
+	@NotBlank
+	private String modelCar;
+	@NotBlank
+	private String responsibleName;
+	@NotBlank
+	private String apartment;
+	@NotBlank
+	private String block;
+
+}
---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,23 @@
+package com.api.parkingcontrol.services;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+	
+	final ParkingSpotRepository parkingSpotRepository;
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+	
+	@Transactional
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		// TODO Auto-generated method stub
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
+}
